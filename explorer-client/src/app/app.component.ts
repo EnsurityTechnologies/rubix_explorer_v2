7432848a1616af74074ff349ed0842f452e8aa59
Rajashekar <Rajashekar@DESKTOP-K97MTSM>
2021-11-03 03:14:17 -0700

    added UI

--- explorer-client/src/app/app.component.ts
+++ explorer-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,89 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'explorer-client';
+
+  transCharthighcharts = Highcharts;
+  tokensCharthighcharts = Highcharts;
+  usersCharthighcharts = Highcharts;
+
+  transchartOptions: Highcharts.Options = {
+    title: {
+      text: "Average Transactions",
+    },
+    xAxis: {
+      title: {
+        text: 'Months'
+      },
+      categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    },
+    yAxis: {
+      title: {
+        text: "Transactions"
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    series: [{
+      name: 'Count',
+      data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 24.4, 19.3, 16.0, 18.4, 17.9],
+      type: 'line',
+      showInLegend: false,
+    }]
+  }
+
+  tokenschartOptions: Highcharts.Options = {
+    title: {
+      text: "Average Tokens"
+    },
+    xAxis: {
+      title: {
+        text: 'Months'
+      },
+      categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    },
+    yAxis: {
+      title: {
+        text: "Tokens"
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    series: [{
+      name: 'Count',
+      data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 24.4, 19.3, 16.0, 18.4, 17.9],
+      type: 'column',
+      showInLegend: false,
+    }]
+  }
+  
+  userschartOptions: Highcharts.Options = {
+    title: {
+      text: "Average Tokens"
+    },
+    xAxis: {
+      title: {
+        text: 'Months'
+      },
+      categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    },
+    yAxis: {
+      title: {
+        text: "Tokens"
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    series: [{
+      name: 'Count',
+      data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 24.4, 19.3, 16.0, 18.4, 17.9],
+      type: 'pie',
+      showInLegend: false,
+    }]
+  }
 }
