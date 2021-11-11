e927f5b49aacd7140cf3477c489327c8309fefe1
Rajashekar <Rajashekar@DESKTOP-K97MTSM>
2021-11-11 18:15:08 +0530

    updated the date

--- Loadertest/Program.cs
+++ Loadertest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
+using Rubix.API.Shared;
 using Rubix.API.Shared.Entities;
 using System;
 using System.Collections.Generic;
@@ -45,28 +46,63 @@ namespace Loadertest
                 //Console.WriteLine(month3results);
 
                 // Days - Week
-                long total = 0;
-                for(int i=1; i <= 7;i ++)
-                {
-                    var date = DateTime.Today.AddDays(-i);
-                    var nextDate = date.AddDays(1);
-                    var dayCount = await collection.AsQueryable().Where(x => x.CreationTime >= date && x.CreationTime < nextDate).CountAsync();
-                    total= total+dayCount;
-                }
-                Console.WriteLine("**************");
-                Console.WriteLine(total);
+               
+                
+                //long total = 0;
+                //for(int i=1; i <= 7;i ++)
+                //{
+                //    var date = DateTime.Today.AddDays(-i);
+                //    var nextDate = date.AddDays(1);
+                //    var dayCount = await collection.AsQueryable().Where(x => x.CreationTime >= date && x.CreationTime < nextDate).CountAsync();
+                //    total= total+dayCount;
+                //}
+                //Console.WriteLine("**************");
+                //Console.WriteLine(total);
 
-                var WeekRecords=  DateTime.Today.AddDays(-7);
-                var dayCounts = await collection.AsQueryable().Where(x => x.CreationTime <= DateTime.Today && x.CreationTime >= WeekRecords).CountAsync();
-                Console.WriteLine(dayCounts);
-                Console.WriteLine("**************");
+                //var WeekRecords=  DateTime.Today.AddDays(-7);
+                //var dayCounts = await collection.AsQueryable().Where(x => x.CreationTime <= DateTime.Today && x.CreationTime >= WeekRecords).CountAsync();
+                //Console.WriteLine(dayCounts);
+                //Console.WriteLine("**************");
 
 
                 //Weeks - Month records
 
+                DateTime currentDate = DateTime.Today;
+                DateTime anotherDate = currentDate.AddMonths(-1);
+                DayOfWeek weekName = anotherDate.DayOfWeek;
+                int totalWeeksPerMonth = currentDate.WeekdayCount(anotherDate, weekName);
+                Console.WriteLine();
+
+                int monthCount = 0;
+                var tempDate = anotherDate;
+                for (int i = 1; i <= totalWeeksPerMonth; i++)
+                {
+                     Console.WriteLine("Week:"+i);
+
+
+                        if(i==1)
+                        {
+                         tempDate = anotherDate;
+                        }
+
+                        var WeekStartDate= tempDate;
+                        var WeekEndDate = WeekStartDate.AddDays(7);
+
+                        var data= await collection.AsQueryable().Where(x => x.CreationTime >= WeekStartDate && x.CreationTime < WeekEndDate).CountAsync();
+                        Console.WriteLine(data);
+
+                        monthCount = monthCount + data;
+                        tempDate = WeekEndDate;  
+                }
+
+                Console.WriteLine(monthCount);
+
+                var totalCount = await collection.AsQueryable().Where(x => x.CreationTime <= currentDate && x.CreationTime >= anotherDate).CountAsync();
+                Console.WriteLine(totalCount);
+                Console.WriteLine("**************");
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
--- Rubix.API.Shared/DateTimeExtensions.cs
+++ Rubix.API.Shared/DateTimeExtensions.cs
@@ -17,5 +17,37 @@ namespace Rubix.API.Shared
             }
             return dt.AddDays(-1 * diff).Date;
         }
+
+        /// <summary>        
+        /// Returns the range of dates of the specified weekday
+        /// between the current System.DateTime object and another date.
+        /// </summary>
+        /// <param name="currentDate">The current date</param>
+        /// <param name="anotherDate">The other date of the interval to search</param>
+        /// <param name="dayOfWeek">The weekday to find</param>
+        /// <returns>The collection of dates of the specified weekday within the period.
+        /// </returns>
+        public static IEnumerable<DateTime> Weekdays(this DateTime currentDate,
+                      DateTime anotherDate, DayOfWeek dayOfWeek)
+        {
+            int sign = anotherDate.CompareTo(currentDate);
+            return Enumerable.Range(0, sign * anotherDate.Subtract(currentDate).Days)
+                .Select(delta => currentDate.AddDays(sign * (1 + delta)))
+                .Where(date => date.DayOfWeek == dayOfWeek);
+        }
+
+        /// <summary>
+        /// Returns the count of the specified weekday between
+        /// the current System.DateTime object and another date.
+        /// </summary>
+        /// <param name="currentDate">The current date</param>
+        /// <param name="anotherDate">The other date of the interval to search</param>
+        /// <param name="dayOfWeek">The weekday to find</param>
+        /// <returns>The count of the specified weekday within the period.</returns>
+        public static int WeekdayCount(this DateTime currentDate,
+                          DateTime anotherDate, DayOfWeek dayOfWeek)
+        {
+            return currentDate.Weekdays(anotherDate, dayOfWeek).Count();
+        }
     }
 }
