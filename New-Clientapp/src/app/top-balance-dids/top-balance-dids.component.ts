a6d41fbd1b338bb9e0affea143b81e455c651e8c
Rajasekhar <rajasekhard2015@outlook.com>
2023-08-30 14:20:14 +0530

    added the excel export

--- New-Clientapp/src/app/top-balance-dids/top-balance-dids.component.ts
+++ New-Clientapp/src/app/top-balance-dids/top-balance-dids.component.ts
@@ -2,14 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-top-balance-dids',
   templateUrl: './top-balance-dids.component.html',
   styleUrls: ['./top-balance-dids.component.css']
 })
 export class TopBalanceDidsComponent implements OnInit {
-
+  fileName= 'walletbalances.xlsx';
   wallets: any[] = [];
   spinstatus: boolean = true;
   interval: any;
@@ -34,16 +34,30 @@ export class TopBalanceDidsComponent implements OnInit {
     }
     this.loading = true;
     this.spinstatus = true;
-    this.dataService.getTopBalanceUserDIDs(this.pageNumber, this.pageSize).subscribe((data: any) => {
+     this.dataService.getTopBalanceUserDIDs(this.pageNumber, this.pageSize).subscribe((data: any) => {
 
-      this.wallets =  this.wallets.concat(data);
-      this.pageNumber++;
-      this.loading = false;
-      this.spinstatus = false;
-    });
+       this.wallets =  this.wallets.concat(data);
+       this.pageNumber++;
+       this.loading = false;
+       this.spinstatus = false;
+     });
    
   }
   userDidInfo(did: any) {
     this.router.navigate(['/userinfo/' + did]);
+  }
+  exportexcel(): void
+  {
+    /* pass here the table id */
+    let element = document.getElementById('wallet-balances');
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+ 
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+ 
+    /* save to file */  
+    XLSX.writeFile(wb, this.fileName);
+ 
   }
 }
